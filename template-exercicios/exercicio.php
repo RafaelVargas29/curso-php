1b51c1baeb6d53ca00a03cc1b0b1ff047ec79fa9
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-12-10 16:08:18 -0300

    Navegação - v1

--- template-exercicios/exercicio.php
+++ template-exercicios/exercicio.php
@@ -5,15 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/CSS/exercicio.css">
     <link href="https://fonts.googleapis.com/css2?family=Alkalami&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Exercicio</title>
 </head>
-    <body>
+    <body class="exercicio">
         <header class="cabecalho">
             <h1>Curso PHP</h1>
             <h2>Visualização do Exercício</h2>
         </header>
         
+        <nav class="navegacao">
+            <a href="#" class="verde">Sem formatação </a>
+            <a href="index.php" class="vermelho">Voltar</a>
+        </nav>
+
         <main class="principal">
             <div class="conteudo">
                
